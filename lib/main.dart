ce8d03cacb64f88a5f8b49b100eacfa1b59d18e0
jastip <hello@jastip.com>
2019-04-04 23:37:40 +0700

    Demo

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:cubef/cubef.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Cubef Example',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Cubef Widget Example'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -44,68 +26,101 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  GlobalKey<CubefState> cubefKey = new GlobalKey<CubefState>();
+  
+  void rollDown() {
+    cubefKey.currentState.rollDown();
+  }
+
+  void rollUp() {
+    cubefKey.currentState.rollUp();
+  }
+
+  void rollLeft() {
+    cubefKey.currentState.rollLeft();
+  }
+
+  void rollRight() {
+    cubefKey.currentState.rollRight();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    double width = 100.0, height = 100.0;
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Cubef(
+            key: cubefKey,
+            animationEffect: Curves.decelerate,
+            animationDuration: 500,
+            child1: Container(
+              height: height,
+              width: width,
+              decoration: BoxDecoration(
+                color: Colors.black
+              ),
+              child: Text("1"),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
+            child2: Container(
+              height: height,
+              width: width,
+              decoration: BoxDecoration(
+                color: Colors.green[500],
+              ),
+              child: Text("2"),
             ),
-          ],
-        ),
+            child3: Container(
+              height: height,
+              width: width,
+              decoration: BoxDecoration(
+                color: Colors.orange[500],
+              ),
+              child: Text("3"),
+            ),
+            child4: Container(
+              height: height,
+              width: width,
+              decoration: BoxDecoration(
+                color: Colors.blue[500],
+              ),
+              child: Text("4"),
+            ),
+            child5: Container(
+              height: height,
+              width: width,
+              decoration: BoxDecoration(
+                color: Colors.pink[500],
+              ),
+              child: Text("5"),
+            ),
+            child6: Container(
+              height: height,
+              width: width,
+              decoration: BoxDecoration(
+                color: Colors.purple[500],
+              ),
+              child: Text("6"),
+            ),
+            width: 100,
+            height: 100,
+          ),
+          Divider(),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+            RaisedButton(onPressed: rollUp, child: Text('Roll Up')),
+            RaisedButton(onPressed: rollDown, child: Text('Roll Down')),
+            RaisedButton(onPressed: rollLeft, child: Text('Roll Left')),
+            RaisedButton(onPressed: rollRight, child: Text('Roll Right')),
+          ],)
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
